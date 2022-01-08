eb192f1d718163e835d992b285811abf61dca259
Karim <karim.wael@gmail.com>
2022-01-08 08:39:51 +0400

    same as last commit

---
+++ httpd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	dbmanager "gauth.com/httpd/DBManager"
+	"gauth.com/httpd/handler"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	c := dbmanager.StartConnection()
+	c.Query("CREATE TABLE GODRIVER (ID INT PRIMARY KEY, NAME VARCHAR(50), AGE INT);")
+	r.GET("/ping", handler.PingGet())
+	r.Run("localhost:8024")
+}
